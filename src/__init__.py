6e7cf582f2ddaf88087eddd0ca672ab8338b02ff
Natasha P <natashap.manchester2019@gmail.com>
2022-04-03 19:50:40 +0100

    Created database, login and signup

--- src/__init__.py
+++ src/__init__.py
@@ -3,11 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "users.db"
+
 
 # function that creates the application
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "secretkey"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .pages import pages
     from .auth import auth
@@ -15,4 +20,24 @@ def create_app():
     app.register_blueprint(pages, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User
+
+    create_database(app)
+
+    # redirects the user to the login page if they haven't logged in
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    # function for the login manager to find the user model
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+
+# function that creates the database if it doesn't already exist
+def create_database(app):
+    if not path.exists("src/" + DB_NAME):
+        db.create_all(app=app)
--- src/auth.py
+++ src/auth.py
@@ -1,15 +1,58 @@
-
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('pages.dashboard'))
+            else:
+                flash("Incorrect email or password", category='error')
+        else:
+            flash("Incorrect email or password", category='error')
+
     return render_template("login.html")
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        name = request.form.get("name")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        # checks whether a user has already signed up and checks whether their email is stored in the User database
+        email_exists = User.query.filter_by(email=email).first()
+        name_exits = User.query.filter_by(name=name).first()
+        if email_exists:
+            flash('An account with this email already exists', category='error')
+        elif name_exits:
+            flash('An account with this name already exists', category='error')
+        elif password1 != password2:
+            flash('Password does not match', category='error')
+        elif len(password1) < 7:
+            flash('Password is too short', category='error')
+        else:
+            new_user = User(email=email, name=name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User created!')
+            return redirect(url_for('auth.login'))
+
     return render_template("signup.html")
